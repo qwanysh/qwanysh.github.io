7d2c0f1581116a557165ab8402d89f71633a19dc
qwanysh <olimpsdu@gmail.com>
2020-04-03 00:43:17 +0600

    Added swipe event to side menu

--- static/js/side_menu.js
+++ static/js/side_menu.js
@@ -1,4 +1,13 @@
 var menuButton = document.querySelector('.menu-btn');
+var hammertime = new Hammer(document.body);
+
+hammertime.on('swipeleft', function () {
+  document.body.classList.add('side-menu-active');
+});
+
+hammertime.on('swiperight', function () {
+  document.body.classList.remove('side-menu-active');
+});
 
 menuButton.addEventListener('click', function () {
   document.body.classList.toggle('side-menu-active');
